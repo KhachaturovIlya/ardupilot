333e162dab0e8088dff2e8482411534d0cd624e7
TeamBauhaus <khachaturov.ilya@yandex.ru>
2023-12-02 15:58:36 +0300

    ]]]

--- ArduCopter/UserCode.cpp
+++ ArduCopter/UserCode.cpp
@@ -1,17 +1,61 @@
 #include "Copter.h"
 
+#include <AP_HAL/AP_HAL.h>
+
+        // we need a boardconfig created so that the io processor's enable
+        // parameter is available
+#if CONFIG_HAL_BOARD == HAL_BOARD_CHIBIOS
+#include <AP_BoardConfig/AP_BoardConfig.h>
+#include <AP_IOMCU/AP_IOMCU.h>
+AP_BoardConfig BoardConfig;
+#endif
+
 #ifdef USERHOOK_INIT
 void Copter::userhook_init()
 {
-    // put your initialisation code here
-    // this will be called once at start-up
+
 }
 #endif
 
 #ifdef USERHOOK_FASTLOOP
 void Copter::userhook_FastLoop()
 {
-    // put your 100Hz code here
+    void setup();
+    void loop();
+
+    const AP_HAL::HAL& hal = AP_HAL::get_HAL();
+
+    void setup (void)
+    {
+        hal.console->printf("Starting AP_HAL::RCOutput test\n");
+    #if CONFIG_HAL_BOARD == HAL_BOARD_CHIBIOS
+        BoardConfig.init();
+    #endif
+        for (uint8_t i = 0; i< 14; i++) {
+            hal.rcout->enable_ch(i);
+        }
+    }
+
+    static uint16_t pwm = 1500;
+    static int8_t delta = 1;
+
+    void loop (void)
+    {
+        for (uint8_t i=0; i < 14; i++) {
+            hal.rcout->write(i, pwm);
+            pwm += delta;
+            if (delta > 0 && pwm >= 2000) {
+                delta = -1;
+                hal.console->printf("decreasing\n");
+            } else if (delta < 0 && pwm <= 1000) {
+                delta = 1;
+                hal.console->printf("increasing\n");
+            }
+        }
+        hal.scheduler->delay(5);
+    }
+
+    AP_HAL_MAIN();
 }
 #endif
 
