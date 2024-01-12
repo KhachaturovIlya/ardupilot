810b3d642b921d1e0437e59885f101c085b841cf
TeamBauhaus <khachaturov.ilya@yandex.ru>
2024-01-12 18:01:07 +0300

    dada

--- ArduCopter/UserCode.cpp
+++ ArduCopter/UserCode.cpp
@@ -3,19 +3,19 @@
 #ifdef USERHOOK_INIT
 void Copter::userhook_init()
 {
-        uint16_t pwm_wat = 1400;
+    uint16_t pwm_wat = 1400;
 
-        motors->init(AP_Motors::MOTOR_FRAME_HEXA, AP_Motors::MOTOR_FRAME_TYPE_X);
-        motors->is_motor_enabled(5);
-        motors->is_motor_enabled(6);
-        motors->armed(true);
-        motors->set_dt(1.0/400.0);
-        motors->set_update_rate(490);
-        motors->output_min();
+    motors->init(AP_Motors::MOTOR_FRAME_HEXA, AP_Motors::MOTOR_FRAME_TYPE_X);
+    motors->is_motor_enabled(5);
+    motors->is_motor_enabled(6);
+    motors->armed(true);
+    motors->set_dt(1.0/400.0);
+    motors->set_update_rate(490);
+    motors->output_min();
 
-        for(int i = 5; i <= 6; ++i)
-        {
-        motors->rc_write(i, pwm_wat);}
+    for(int i = 5; i <= 6; ++i)
+    {
+    motors->rc_write(i, pwm_wat);}
 }
 #endif
 
@@ -41,15 +41,10 @@ void Copter::userhook_MediumLoop()
 #endif
 
 #ifdef USERHOOK_SLOWLOOP
-///void Copter::userhook_SlowLoop()
-///{
-///    chel += 1;
+void Copter::userhook_SlowLoop()
+{
 
-///    if(chel == 100)
-///    {
-
-///    }
-///}
+}
 #endif
 
 #ifdef USERHOOK_SUPERSLOWLOOP
