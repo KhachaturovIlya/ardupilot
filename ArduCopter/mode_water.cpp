6e777647bafc4d47910ee0676f7cae41972abbe4
TeamBauhaus <khachaturov.ilya@yandex.ru>
2024-01-13 11:27:01 +0300

    dada

--- ArduCopter/mode_water.cpp
+++ ArduCopter/mode_water.cpp
@@ -12,13 +12,16 @@ void ModeWater::run()
 
 
     motors->init(AP_Motors::MOTOR_FRAME_HEXA, AP_Motors::MOTOR_FRAME_TYPE_X);
-    motors->is_motor_enabled(5);
-    motors->is_motor_enabled(6);
-    motors->armed(true);
-    motors->set_dt(1.0/400.0);
-    motors->set_update_rate(490);
-    motors->output_min();
+    for (uint8_t i = 0; i < AP_MOTORS_MAX_NUM_MOTORS; ++i) {
+        if (!motors->is_motor_enabled(i)) {
+            continue;
+        }
 
-    attitude_control->set_throttle_out(1.0f, true, g.throttle_filt);
+        const Vector2f output{motors->get_roll_factor(i), motors->get_pitch_factor(i)};
+
+        int16_t pwm = motors->get_pwm_output_min() + (motors->get_pwm_output_max() - motors->get_pwm_output_min()) * 0.5;
+
+        motors->rc_write(i, pwm);
+    }
 
 }
