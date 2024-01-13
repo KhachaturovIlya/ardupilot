c3c0b3aa1de4ce2e45d2f2222ecb283700da91b1
TeamBauhaus <khachaturov.ilya@yandex.ru>
2024-01-13 10:45:52 +0300

    dada

--- ArduCopter/UserCode.cpp
+++ ArduCopter/UserCode.cpp
@@ -3,19 +3,7 @@
 #ifdef USERHOOK_INIT
 void Copter::userhook_init()
 {
-    uint16_t pwm_wat = 1400;
 
-    motors->init(AP_Motors::MOTOR_FRAME_HEXA, AP_Motors::MOTOR_FRAME_TYPE_X);
-    motors->is_motor_enabled(5);
-    motors->is_motor_enabled(6);
-    motors->armed(true);
-    motors->set_dt(1.0/400.0);
-    motors->set_update_rate(490);
-    motors->output_min();
-
-    for(int i = 5; i <= 6; ++i)
-    {
-    motors->rc_write(i, pwm_wat);}
 }
 #endif
 
