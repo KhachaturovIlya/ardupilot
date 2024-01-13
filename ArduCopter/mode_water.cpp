8740df4940101461ca7bf1b2d72287e5aed6b8ce
TeamBauhaus <khachaturov.ilya@yandex.ru>
2024-01-13 11:32:31 +0300

    Dadad

--- ArduCopter/mode_water.cpp
+++ ArduCopter/mode_water.cpp
@@ -10,16 +10,14 @@ void ModeWater::run()
 {
     ///    static uint16_t pwm_wat = 1600;
 
-
     motors->init(AP_Motors::MOTOR_FRAME_HEXA, AP_Motors::MOTOR_FRAME_TYPE_X);
-    for (uint8_t i = 0; i < AP_MOTORS_MAX_NUM_MOTORS; ++i) {
+
+    for (uint8_t i = 5; i <= 6; ++i) {
         if (!motors->is_motor_enabled(i)) {
             continue;
         }
 
-        const Vector2f output{motors->get_roll_factor(i), motors->get_pitch_factor(i)};
-
-        int16_t pwm = motors->get_pwm_output_min() + (motors->get_pwm_output_max() - motors->get_pwm_output_min()) * 0.5;
+        int16_t pwm = motors->get_pwm_output_min() + (motors->get_pwm_output_max() - motors->get_pwm_output_min()) * 0.5f;
 
         motors->rc_write(i, pwm);
     }
