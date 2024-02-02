47b36e867a4fa80b0fc08d0aaa0e35e26c786bad
TeamBauhaus <khachaturov.ilya@yandex.ru>
2024-02-02 16:44:39 +0300

    dad

--- build/Pixhawk1/ap_version.h
+++ build/Pixhawk1/ap_version.h
@@ -6,7 +6,7 @@
 #error ap_version.h should never be included directly. You probably want to include AP_Common/AP_FWVersion.h
 #endif
 
-#define GIT_VERSION "2fd83b78"
-#define GIT_VERSION_INT 802700152
+#define GIT_VERSION "58208730"
+#define GIT_VERSION_INT 1478526768
 #define AP_BUILD_ROOT "/home/milonolim/final/ardupilot"
 #define CHIBIOS_GIT_VERSION "d8c45abd"
