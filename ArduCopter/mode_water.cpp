00f6a7fd30eed86fd9c91e2244e2fc0b90fa30e5
TeamBauhaus <khachaturov.ilya@yandex.ru>
2024-02-10 11:28:28 +0300

    dad

--- ArduCopter/mode_water.cpp
+++ ArduCopter/mode_water.cpp
@@ -9,7 +9,7 @@ void ModeWater::run()
 {
     if (motors->armed()) {
         motors->armed(false);
-        motors->set_desired_spool_state(AP_Motors::DesiredSpoolState::THROTTLE_UNLIMITED);
+        motors->set_desired_spool_state(AP_Motors::DesiredSpoolState::SHUT_DOWN);
     }
     hal.console->begin(115200);
     hal.console->printf("\n\nWater Mode working\n\n");
@@ -18,19 +18,14 @@ void ModeWater::run()
     motors->init(AP_Motors::MOTOR_FRAME_HEXA, AP_Motors::MOTOR_FRAME_TYPE_X);
 
     motors->set_desired_spool_state(AP_Motors::DesiredSpoolState::THROTTLE_UNLIMITED);
-    motors->set_dt(1.0/400.0);
-    motors->set_update_rate(490);
-    motors->output_min();
+    ///motors->output_min();
     motors->armed(true);
-    
-    motors->is_motor_enabled(5);
-    motors->is_motor_enabled(6);
 
     for (uint8_t i = 1; i <= 6; ++i) {
         ///SRV_Channel::Aux_servo_function_t function = SRV_Channels::get_motor_function(i);
         ///SRV_Channels::set_aux_channel_default(function, i);
         
-
+        motors->is_motor_enabled(i);
         motors->rc_write(i, pwm_wat);
     }
 
